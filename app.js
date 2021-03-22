d396e9fd165e64461f7a1c22ce2f20f1efe08b9c
Petr Stratonnikov <pstratonni@gmail.com>
2021-03-22 23:19:20 +0100

    rew

---
+++ app.js
@@ -0,0 +1,64 @@
+// Первое задание
+// let numer;
+// const m = 10;
+// while (true) {
+//   numer = +prompt("Введите число");
+//   if (numer < m) {
+//     alert("Меньше");
+//   } else if (numer > m) {
+//     alert("Больше");
+//   } else {
+//     alert("Вы угадали!");
+//     break;
+//   }
+// }
+
+
+
+//Второе задание
+// let multiplicationTable = [
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+//   ["", "", "", "", "", "", "", "", ""],
+// ];
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     multiplicationTable[i][j] = (i + 1) * (j + 1);
+//   }
+// }
+// console.table(multiplicationTable);
+
+
+
+//Третье задание
+// let teams = ["Заря", "Буревестник", "Вымпел", "Ураган"];
+// let resultTable = [
+//   ["", "", "", "", ""],
+//   ["", "", "", "", ""],
+//   ["", "", "", "", ""],
+//   ["", "", "", "", ""],
+//   ["", "", "", "", ""],
+// ];
+// for (let i = 0; i < teams.length + 1; i++) {
+//   for (let j = 0; j < teams.length + 1; j++) {
+//     if (i === 0 && j === 0) {
+//       resultTable[i][j] = "X";
+//       continue;
+//     }
+//     if (i === 0) {
+//       resultTable[i][j] =resultTable[j][i] =teams[j - 1];
+//     }
+//     if (i === j) {
+//       resultTable[i][j] = "X";
+//     }
+//     if(i>0&&j>i){resultTable[i][j]=resultTable[j][i]=`${Math.floor(Math.random()*Math.floor(3))} - ${Math.floor(Math.random()*Math.floor(3))}`}
+//   }
+// }
+// console.table(resultTable);
